78e33b3d5f00f4383b25e778a680e0f87fdfca83
Scott <yusufs3435@gmail.com>
2023-11-17 22:22:10 +0100

    scroll-top button was renewed

--- src/components/common/scroll-to-top-button.jsx
+++ src/components/common/scroll-to-top-button.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { FaArrowUp } from "react-icons/fa";
 import "./scroll-to-top-button.scss";
 import { scrollTop } from "../../helpers/scroll";
 
 const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
+  // sayfada hareketimizi algılayan ve state'yi buna göre değiştiren yer.
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-    const handleClick = () => { 
-        scrollTop("smooth")
-     }
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const handleClick = () => {
+    scrollTop("smooth");
+  };
 
   return (
-    <Button className="btn btn-primary btn-scroll" onClick={handleClick}>
+    <Button className={`btn btn-primary btn-scroll ${isVisible ? 'visible' : 'hidden'}`} onClick={handleClick}>
       <FaArrowUp />
     </Button>
   );
